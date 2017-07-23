09c670b16575b6988415a9d8525261291cae6cc3
Nico Sta. Ana <Nico Sta. Ana>
2017-07-23 14:15:58 -0400

    started working on folder structure

---
+++ models/Article.js
@@ -0,0 +1,28 @@
+// Include the Mongoose Dependencies
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+//Create Schema for Saved Articles
+var Article = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  url:{
+  	type: String,
+  	required: true,
+  	unique: true
+  }
+});
+
+// Create the Model
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export it for use elsewhere
+module.exports = Article;
+
